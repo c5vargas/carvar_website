be97bdb8f2dfdb56e5cc8a471f0d36239fd52606
Carles Vargas <carlesvargas5@gmail.com>
2024-05-09 18:06:40 +0200

    feat: logo updated

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,13 +1,13 @@
 import { Outlet } from "react-router-dom";
-import logo from "../assets/react.svg"
+import logo from "../assets/carvar.svg"
 import Socials from "../components/Socials";
 export function RootLayout() {
 	return (
 		<>
-			<header className="flex items-center justify-between px-10 py-4 w-full bg-gray-900 fixed z-20 top-0 left-0 bg-opacity-50">
-				<picture>
-					<img src={logo} className="w-14" alt="Logo" />
-				</picture>
+			<header className="flex items-center justify-between px-10 py-4 w-full bg-gray-950 fixed z-20 top-0 left-0 bg-opacity-75">
+				<figure>
+					<img src={logo} className="w-32 h-14" alt="Logo" />
+				</figure>
 
 				<Socials />
 			</header>
@@ -15,10 +15,6 @@ export function RootLayout() {
 			<main className="w-full">
 				<Outlet />
 			</main>
-
-			{/* <footer className="fixed p-2 bg-gray-50 shadow bottom-0 left-0 w-full z-20">
-				Lorem ipsum footer
-			</footer> */}
 		</>
 	)
 }
